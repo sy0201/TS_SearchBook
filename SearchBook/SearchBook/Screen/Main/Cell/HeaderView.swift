7b2a707a28c15150a06b7b4f3feb5275a5890164
sy0201 <noey0201@gmail.com>
2024-12-29 11:29:44 +0900

    [DESIGN] : HeaderViewUI 추가

--- SearchBook/SearchBook/Screen/Main/Cell/HeaderView.swift
+++ SearchBook/SearchBook/Screen/Main/Cell/HeaderView.swift
@@ -6,7 +6,35 @@
 //
 
 import UIKit
+import SnapKit
 
 final class HeaderView: UICollectionReusableView, ReuseIdentifying {
+    let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.font = .systemFont(ofSize: 20, weight: .semibold)
+        titleLabel.textColor = .black
+        return titleLabel
+    }()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setupConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension HeaderView {
+    func setupUI() {
+        addSubview(titleLabel)
+    }
+    
+    func setupConstraint() {
+        titleLabel.snp.makeConstraints { make in
+            make.leading.top.trailing.equalToSuperview()
+        }
+    }
 }
